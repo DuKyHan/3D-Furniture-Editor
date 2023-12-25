c37122e5612dd9bd877af23aaf3450b8608bfcb9
Thai Gia Lac <lacthai08@gmail.com>
2023-12-25 10:03:00 +0700

    udapted Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,51 @@
 import React, { useState } from "react";
 import Navbar from "../components/Shop/Navbar";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 
 function Home() {
+
+  const dataBannerHome = [
+    {
+      id: 1,
+      linkImg:
+        "https://images.unsplash.com/photo-1606744824163-985d376605aa?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: 2,
+      linkImg:
+        "https://images.unsplash.com/photo-1618221195710-dd6b41faaea6?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: 3,
+      linkImg:
+        "https://images.unsplash.com/photo-1618219740975-d40978bb7378?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: 4,
+      linkImg:
+        "https://images.unsplash.com/photo-1600607687920-4e2a09cf159d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: 5,
+      linkImg:
+        "https://images.unsplash.com/photo-1604014237800-1c9102c219da?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  ];
+
   return (
     <main className="w-full h-full">
       <Navbar />
+      <Carousel 
+        showThumbs={false}
+      className=" ">
+        {dataBannerHome.map((item) => (
+          <div className="h-[700px]">
+            <img src={item.linkImg} alt="images banner" className=" object-contain"/>
+          </div>
+        ))}
+      </Carousel>
     </main>
   );
 }
